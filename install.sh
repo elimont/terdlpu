a43cdc53c71fcdc6ded99d3b861cd6f89649b6b1
elimont <30360365+elimont@users.noreply.github.com>
2023-05-20 12:30:23 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,12 +5,12 @@ NC='\e[0m'
 YTDLP_CONFIG_FOLDER="${HOME}/.config/yt-dlp/"
 TERMUXURLOPENER_CONFIG_FOLDER="${HOME}/bin/"
 
-echo "Hi, This script setup an environment to download various videos from various apps"
+echo "Hi, This script sets up an environment to download various videos from various apps."
 sleep 1
 echo -e "\n\n${BLUE}Requirements:${NC}"
 echo "    1. Allow storage access to Termux."
 echo "    2. A working internet connection."
-read -p "When you are ready just press enter:"
+read -p "When you are ready, just press enter:"
 
 # Basic setup
 termux-setup-storage
@@ -20,8 +20,11 @@ pkg install python ffmpeg
 pip install -U pip
 pip install -U wheel
 pip install -U yt-dlp
-mkdir -p $TERMUXURLOPENER_CONFIG_FOLDER
-cp -r -u $YTDLP_CONFIG_FOLDER ~/.config/
+mkdir -p "$TERMUXURLOPENER_CONFIG_FOLDER"
+cp -ru "$YTDLP_CONFIG_FOLDER" ~/.config/
 cp termux-url-opener "${TERMUXURLOPENER_CONFIG_FOLDER}/"
 
 echo -e "${BLUE}Congratulations!!! Your setup is complete.${NC}\n\n"
+
+# Run update.sh script
+source "$(dirname "$0")/update.sh"
